db369d9c4707ed918aef799cf6cc6bb88f0941f8
Brian Washington <bw49078@gmail.com>
2022-06-23 01:35:05 -0500

    squashed bugs

--- script.js
+++ script.js
@@ -1,14 +1,5 @@
-let items = []
-const add = document.getElementById("addBtn")
-const clear = document.getElementById("clearBtn")
-const UList = document.getElementById("list")
-const input = document.getElementById("itemEl")
+const add = document.getElementById("addBtn");
 
 add.addEventListener('click', function() {
-    items.push(input.ariaValueNow)
-    input.value = " ";
-})
-
-clear.addEventListener('dblclick', function() {
-
+    console.log("click");
 })
