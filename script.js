9a569426801cedce69eb2582de71f72ebfca319c
Brian Washington <bw49078@gmail.com>
2022-06-23 01:59:17 -0500

    can add input to list and can clear it

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
 const add = document.getElementById("addBtn");
+const clear = document.getElementById("clearBtn");
+let ulEl = document.getElementById("list");
+let items = document.getElementById("itemEl");
 
 add.addEventListener('click', function() {
-    console.log("click");
+    let item = items.value;
+    const li = document.createElement('li');
+    li.innerHTML = item;
+    ulEl.appendChild(li);
+})
+
+clear.addEventListener('dblclick', function() {
+    ulEl.innerHTML = '';
 })
